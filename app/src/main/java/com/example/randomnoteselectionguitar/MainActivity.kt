b9ae73e2928569e6d0b960f85152b4f6634ab23b
Cheng Li <cli130103@gmail.com>
2022-04-27 00:45:51 +1000

    Initial commit

---
+++ app/src/main/java/com/example/randomnoteselectionguitar/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.randomnoteselectionguitar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+
+
+
+
+
+    private fun rollDice() {
+        val x = when((1..12).random()) {
+            1 -> "A"
+            2 -> "A sharp/B flat"
+            3 -> "B"
+            4 -> "C"
+            5 -> "C Sharp/D Flat"
+            6 -> "D"
+            7 -> "D sharp/E Flat"
+            8 -> "E"
+            9 -> "F"
+            10 -> "F sharp/G Flat"
+            11 -> "G"
+            else -> "G Sharp/A Flat"
+        }
+
+
+
+        val resultTextView: TextView = findViewById(R.id.textView)
+
+        resultTextView.text = x + " on fret " + (1..6).random().toString()
+    }
+
+
+}
